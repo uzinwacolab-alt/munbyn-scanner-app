a1b7639cde846476cfee6b7f4cc03c13ab2ae0c2
uzinwacolab-alt <uzinwacolab@gmail.com>
2026-03-06 14:32:44 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'munbyn-scanner-v0.1';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json'
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json'
 ];
 
 // Install Event: Caches all essential files
